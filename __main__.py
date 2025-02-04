5ad7c17b5706cd7acb44977a77e175888fa1cabc
marycamara <marycamara101@gmail.com>
2025-02-04 22:45:42 +0000

    Add Blackjack menu with options to start game, view rules, or exit

--- __main__.py
+++ __main__.py
@@ -4,15 +4,4 @@ from src.menu import BlackjackMenu
 
 
 def main():
-    """ Start the Blackjack game menu. """
-    menu = BlackjackMenu() # Instance for the menu
-    choice = menu.display_menu() # Get user's choice 
-    
-    if choice == "1":
-        """ Starts the Blackjack game if choosen  """
-    game = Game() #  Create an instance for the game 
-    game.play_game() # Runs the game after the menu has been display and the game option has been choosen. 
-  
-
-if __name__ == "__main__":
-    main() # sStarts the game after menu interaction 
+    """ Start the Blackjack game menu. 
